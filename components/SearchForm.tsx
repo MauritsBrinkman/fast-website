d5186a47168db3fbb7ac39518106d1623af25a93
MauritsBrinkman <maurits@captide.co>
2024-07-10 13:59:54 +0200

    Enable search by URL manipulation via searchbar.

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,11 +1,39 @@
 "use client";
 
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { Input } from "@/components/ui/input"
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+import { Input } from "@/components/ui/input";
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { formUrlQuery } from '@/sanity/utils';
 
 const SearchForm = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathname = usePathname();
     const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            let newUrl = '';
+
+            if (search) {
+                const newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'query',
+                    value: search,
+                });
+            } else {
+                newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ['query'],
+                });
+            }
+
+            router.push(newUrl, { scroll: false });
+        }, 300)
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [search])
 
     return (
         <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
